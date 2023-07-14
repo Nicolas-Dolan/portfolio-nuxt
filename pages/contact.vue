fde93ee183eb099c50054d29f3d442c687fa5617
Nicolas-Dolan <nicolas.dolan@ymail.com>
2023-07-14 13:46:31 +0100

    completed contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,53 +1,63 @@
 <template>
   <Slide key="contact">
-    <div class="col-sm">
+    <div class="col-sm-6">
+      <h2>Contact</h2>
+
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <svg
+          fill="white"
+          version="1.1"
+          id="Icons"
+          viewBox="0 0 32 32"
+          xml:space="preserve"
+          height="1rem"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <g>
+              <g>
+                <path
+                  d="M16,16.8l13.8-9.2C29.2,5.5,27.3,4,25,4H7C4.7,4,2.8,5.5,2.2,7.6L16,16.8z"
+                ></path>
+              </g>
+              <g>
+                <path
+                  d="M16.6,18.8C16.4,18.9,16.2,19,16,19s-0.4-0.1-0.6-0.2L2,9.9V23c0,2.8,2.2,5,5,5h18c2.8,0,5-2.2,5-5V9.9L16.6,18.8z"
+                ></path>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <span> nicolas.dolan@ymail.com</span>
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <a
+          class="text-white"
+          href="https://github.com/Nicolas-Dolan"
+          target="_blank"
+          rel="noopener noreferrer"
+          ><i class="devicon-github-plain"></i> github.com/Nicolas-Dolan</a
+        >
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <a
+          class="text-white"
+          href="https://www.linkedin.com/in/nicolas-dolan/"
+          target="_blank"
+          rel="noopener noreferrer"
+          ><i class="devicon-linkedin-plain"></i>
+          linkedin.com/in/nicolas-dolan/</a
+        >
       </p>
     </div>
-    <div class="col-sm align-self-start">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div class="col-sm-6 align-self-start d-flex justify-content-center">
+      <p class="d-sm-block d-none">Location:</p>
+      <img class="earth" alt="earth" src="../assets/images/earth.png" />
     </div>
   </Slide>
 </template>
@@ -57,3 +67,13 @@ import { pageMeta } from "../utils";
 
 definePageMeta(pageMeta as any);
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+.earth {
+  height: 350px;
+}
+</style>

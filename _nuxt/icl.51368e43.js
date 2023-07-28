95822c13c8f10e3ec6478816fab9e71a7fdc443a
Nicolas-Dolan <nicolas.dolan@ymail.com>
2023-07-28 17:04:05 +0100

    Updates

--- _nuxt/icl.51368e43.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./entry.835555d2.js";const t={};function r(a,n){return o(),c("p",null,"Click a project above to view details")}const l=e(t,[["render",r]]);export{l as default};
---
+++ _nuxt/icl.996cbf6c.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./entry.b7a57d8c.js";const t={};function r(a,n){return o(),c("p",null,"Click a project above to view details")}const l=e(t,[["render",r]]);export{l as default};

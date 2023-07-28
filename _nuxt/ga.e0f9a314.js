07ea0ce5ba20fe36f7f0e773ae1762357f7a9210
Nicolas-Dolan <nicolas.dolan@ymail.com>
2023-07-28 14:38:54 +0100

    Updates

---
+++ _nuxt/ga.e0f9a314.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./entry.2cbac29e.js";const t={};function a(r,n){return o(),c("p",null,"Click a project above to view details")}const _=e(t,[["render",a]]);export{_ as default};
---
+++ _nuxt/index.636e8553.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.2cbac29e.js";const o={};function t(r,a){return n(),c("p",null,"Click an icon to view the project")}const _=e(o,[["render",t]]);export{_ as default};
